428af390f45b4f78dd368f12251a88ecef25b5b1
junealexis13 <junealexis.santos13@gmail.com>
2023-06-05 07:43:44 +0800

    color change v1

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline({defaults: {duration:2, ease: "power3.out"}})
 // banner image rotate
-tl.fromTo(".banner-image",{opacity:0, x:-90, rotation: "-90deg", ScrollTrigger:".banner-image"},{opacity:1, x:0, rotation:"0deg"})
-tl.fromTo("div.banner-element2",{scale:0},{scale:1, ease: "elastic.out(1, 0.3)", delay: 0.3},"<")
+tl.fromTo(".banner-image",{opacity:0, x:-90, rotation: "-90deg"},{opacity:1, x:0, rotation:"0deg"})
+tl.fromTo("div.banner-element2",{x:200, opacity:0},{x:0, opacity:1, delay: 0.3},"<")
