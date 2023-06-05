1cac271f29ca08d12303ff04028f9ab28a687d86
junealexis13 <junealexis.santos13@gmail.com>
2023-06-05 09:37:59 +0800

    design v2

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline({defaults: {duration:2, ease: "power3.out"}})
 // banner image rotate
-tl.fromTo(".banner-image",{opacity:0, x:-90, rotation: "-90deg"},{opacity:1, x:0, rotation:"0deg"})
+tl.fromTo(".banner-image",{opacity:0, x:-90, rotation: "-45deg", scale: 0.5},{opacity:1, x:0, rotation:"0deg", scale:1, ease: "elastic.out(1, 0.5)" })
 tl.fromTo("div.banner-element2",{x:200, opacity:0},{x:0, opacity:1, delay: 0.3},"<")
