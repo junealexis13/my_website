fbd0a573e83e161b18679a7f180ebaf41337499c
junealexis13 <junealexis.santos13@gmail.com>
2023-06-07 20:04:59 +0800

    v4.3.1

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ gsap.registerPlugin(ScrollTrigger);
 // Wiping animation
 const tl = gsap.timeline({defaults: {duration:1, ease: "power3.out"}})
 
-
 // getting the viewport dimensions
 var viewportWidth = window.innerWidth || document.documentElement.clientWidth
 var viewportHeight = window.innerHeight || document.documentElement.clientHeight
+
+// Adding event listener to clicks to change the page
+document.getElementById("link-research").addEventListener("click", function() {window.location.href = "./page1.html"});
+document.getElementById("link-home").addEventListener("click", function() {window.location.href = "./index.html"});
+    
 
 tl.fromTo(".banner-image",{opacity:0, x:-90, rotation: "-45deg", scale: 0.5},{opacity:1, x:0, rotation:"0deg", scale:1, ease: "elastic.out(1, 0.5)" })
 tl.fromTo("div.banner-element2",{x:200, opacity:0},{x:0, opacity:1, delay: 0.3})
