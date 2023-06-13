e6bad25b43950eda239ed2a95d4ede88cb6dcdf3
junealexis13 <junealexis.santos13@gmail.com>
2023-06-13 12:27:09 +0800

    minor update

--- about_me_animation.js
+++ about_me_animation.js
@@ -1,17 +1,17 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.from("svg.toc-mark1",{scrollTrigger:{trigger: "div.about-me-narrative", start: "top 0%", end: "bottom 0%",  markers:false, toggleActions: "restart reverse restart reverse"},
-        scale:0.5, ease: "power4.out"});
+        scale:0.5, duration: 0.2,  ease: "elastic.out(1, 0.4)"});
 
 
 gsap.from("svg.toc-mark2",{scrollTrigger:{trigger: "div.education-timeline", markers:false, start: "top 0%", end: "bottom 0%",  toggleActions: "restart reverse restart reverse"},
-        scale:0.5, ease: "power4.out"});
+        scale:0.5, duration: 0.2, ease: "elastic.out(1, 0.4)"});
 
 gsap.from("svg.toc-mark3",{scrollTrigger:{trigger: "div.career-timeline", markers:false, start: "top 0%", end: "bottom 0%", toggleActions: "restart reverse restart reverse"},
-        scale:0.5, ease: "power4.out"});
+        scale:0.5, duration: 0.2, ease: "elastic.out(1, 0.4)"});
 
 gsap.from("svg.toc-mark4",{scrollTrigger:{trigger: "div.milestone-timeline", markers:false, start: "top 0%", end: "bottom 0%", toggleActions: "restart reverse restart reverse"},
-        scale:0.5, ease: "power4.out"});
+        scale:0.5, duration: 0.2, ease: "elastic.out(1, 0.4)"});
 
 gsap.from("svg.toc-mark5",{scrollTrigger:{trigger: "div.others-timeline", markers:false, start: "top 0%", end: "bottom 0%", toggleActions: "restart reverse restart reverse"},
-        scale:0.5, ease: "power4.out"});
+        scale:0.5, duration: 0.2, ease: "elastic.out(1, 0.4)"});
